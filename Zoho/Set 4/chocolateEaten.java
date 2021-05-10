ccc1210a74ae1eaf5d0f5b60f2f3da65bf006a34
Siva prakash <30351639+Sivaprakash22@users.noreply.github.com>
2021-05-10 23:24:02 +0530

    Set 4

--- Zoho/Set 4/chocolateEaten.java
+++ Zoho/Set 4/chocolateEaten.java
@@ -1,19 +1,25 @@
-public class chocolateEaten {
-    public static void main(String args[])
-    {
-        int money = 40,price = 1,wrappers = 3,chocolate = 1;
-      //  int maxVisit = 1;
+import java.util.*;
 
-        int chocolateEaten = money / price;
-        int wrappersLeftInHand = money / (chocolate * wrappers);
-
-        chocolateEaten += wrappersLeftInHand;
-   
-        System.out.print(
-         "Total chocolate can eat "+chocolateEaten+'\n'+
-         "Wrappers left in hand "+Math.abs(wrappersLeftInHand)
-
-        );
+public class chocolateEaten
+{  
+  public static void main(String args[])
+  {  
+    int money = 50, price =1, wrappers =3, choco = 1, maxVisit = 0;
+    int eaten = 0, wrapperInHand = 0;
+    do{
+        if(money>0) {
+            eaten+=(money/price);
+            wrapperInHand += eaten;
+            money = 0;
+        }
+        if(wrapperInHand > wrappers) {
+            int exchangedChoco = (wrapperInHand/wrappers) * choco;
+            eaten+= exchangedChoco;
+            wrapperInHand %= wrappers;
+            wrapperInHand += exchangedChoco;
+        }
         
-    }
+    } while(--maxVisit!=0);
+    System.out.println(eaten+" "+wrapperInHand);
+  }
 }
