1953fce04cdb8b586a4605ba86831a7a849fa240
Siva prakash <30351639+Sivaprakash22@users.noreply.github.com>
2021-10-17 19:39:20 +0530

    Update pattern3.java

--- Pattern/pattern3.java
+++ Pattern/pattern3.java
@@ -3,6 +3,18 @@ class pattern3
 {
     public static void main(String args[])
     {
+        
+        
+// Same Logic  
+//-----------
+//         int n=5;
+//         for(int i=0;i<=n;i++)
+//         {
+//             System.out.println("ABCDE");
+//         }
+        
+        
+        
         int n=5;
         char alpha = 'A';
         for(int i=1;i<=n;i++)
